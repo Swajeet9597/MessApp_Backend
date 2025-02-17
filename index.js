d4cc8611cd9d9b01e853e3e587735d5e5af492c3
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2025-02-17 11:29:56 +0530

    scasca

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
 const corsOption = {
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
     methods:"POST,GET,PATCH,DELETE,HEAD",
     credentials: true,
 }
