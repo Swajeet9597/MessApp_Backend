f68492f84dceb86a31fbef5df64577b152f8cba1
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2025-02-17 11:28:32 +0530

    sdvsdvsd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
 const corsOption = {
-    origin: "http://localhost:4200",
+    origin: "http://localhost:5174",
     methods:"POST,GET,PATCH,DELETE,HEAD",
     credentials: true,
 }
