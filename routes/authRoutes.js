5697bfa8a3ab02369a7f0785129705fe9ef76938
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2025-03-08 11:02:49 +0530

    addRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const { addUser, loginUser,getUser, logoutUser, messFormRendering } = require("../controllers/authControllers")
 const messFormAccessMiddleware = require("../middlewares/messFormAccessMiddleware")
+const authMiddleware = require("../middlewares/authMiddleware")
 
 const router = express.Router()
 
 
 router.post("/register",addUser)
 router.post("/login",loginUser)
+router.get("/auth",authMiddleware,(req,res)=>{
+    // res.status(200).json({
+    //     success: true,
+    //     msg:"Done"
+    // })
+})
 router.get("/getUser",getUser)
 router.get("/messFormRendering",messFormAccessMiddleware,messFormRendering)
 router.post("/logout",logoutUser)
